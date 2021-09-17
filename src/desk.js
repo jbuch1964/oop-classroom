69527b43a6bbd3de8be6ed223151f4fc8f5e94bc
John Buchanon <jbuch1964@gmail.com>
2021-09-17 17:10:35 -0400

    AfterEdits-OOP-Classroom

--- src/desk.js
+++ src/desk.js
@@ -1,5 +1,22 @@
 class Desk {
-  
+    constructor(length, width, height, isWhiteboard) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        this.isWhiteboard = isWhiteboard;
+        this.content = '';
+    }
+    write(content) {
+        this.content += content;
+    }
+
+    wipe() {
+        if (this.isWhiteboard === true) {
+            this.content = '';
+        } 
+    }
+
 }
+
 
 module.exports = Desk
--- src/marker.js
+++ src/marker.js
@@ -1,4 +1,18 @@
 class Marker {
+    constructor(size, color, remainingInk) {
+        this.size = size;
+        this.color = color;
+        this.remainingInk = remainingInk;
+    }
+
+    write(change) {
+        let change = change.replace(/\s+/g, '');
+        this.remainingInk -= change.length;
+        if (this.remainingInk === 3) {
+               change = 
+        }
+        return change;
+    }
   
 }
 
